d4aa153ae826936e1e5abd21e6a0b63107183be3
thdkans <hsiaoyawen20@gmail.com>
2025-03-07 15:52:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const ROOM_STATUS = {
 };
 
 // Discord bot token and channel ID
-const token = 'MTM0NzExNTU1NzU1OTg2MTMwMQ.GOE-5X.vLQtonSgbrAnVOFBY5vTOwhrFfo-3dcqBZs18M';
-const channelId = '1347039291049775114';
-const roomId = 'leesh2148';
+const token = '';
+const channelId = '';
+const roomId = '';
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
